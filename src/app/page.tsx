3c20049578d7f22720c1db4a8d65e777cc2eef88
Ron Almog <ron8132@gmail.com>
2024-12-20 09:49:14 -0500

    add signin, signup pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      hello
+      <Button>Hello</Button>
+    </div>
+  );
 }
