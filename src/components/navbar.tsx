af8d6bd50c6404ea53ba9909b3f4b9d6274d93cc
Ron Almog <ron8132@gmail.com>
2024-12-22 09:23:58 -0500

    implement sign out

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import { AirVent } from "lucide-react";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div className="border-b px-4">
       <div className="flex items-center justify-between mx-auto max-w-4xl h-16">
@@ -11,9 +17,21 @@ const Navbar = () => {
           <span>BetterAuth Demo</span>
         </Link>
         <div>
-          <Link href="/sign-in" className={buttonVariants()}>
-            Sign In
-          </Link>
+          {session ? (
+            <form
+              action={async () => {
+                "use server";
+                await auth.api.signOut({ headers: await headers() });
+                redirect("/");
+              }}
+            >
+              <Button type="submit">Sign Out</Button>
+            </form>
+          ) : (
+            <Link href="/sign-in" className={buttonVariants()}>
+              Sign In
+            </Link>
+          )}
         </div>
       </div>
     </div>
