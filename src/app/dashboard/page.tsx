07c13fba9b88596b769dd3bc4ccc8988c46e795f
Ron Almog <ron8132@gmail.com>
2024-12-22 09:13:06 -0500

    use getsession on client side

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,22 @@
-const Dashboard = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return redirect("/");
+  }
   return (
     <div className="mt-10 text-center">
       <h1 className="text-2xl font-bold underline">Welcome to dashboard</h1>
+
+      <ul>
+        <li>Name: {session.user.name}</li>
+        <li>Email: {session.user.email}</li>
+      </ul>
     </div>
   );
 };
